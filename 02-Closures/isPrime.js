483b79135675648e0e8a8362ae88d960dd522dda
Magesh <tkmagesh77@gmail.com>
2017-06-08 10:18:50 +0530

    memoize - version 1

--- 02-Closures/isPrime.js
+++ 02-Closures/isPrime.js
@@ -24,7 +24,6 @@ var isPrime = (function (){
 var isPrime = (function (){
 	
 	var cache = {};
-
 	function checkPrime(n){
 		console.log('processing ', n);
 		if (n <= 3){
@@ -37,7 +36,6 @@ var isPrime = (function (){
 		}
 		return true;
 	}
-
 	return function(n){
 		if (typeof cache[n] === 'undefined')
 			cache[n] = checkPrime(n);	
@@ -46,8 +44,33 @@ var isPrime = (function (){
 	
 })();
 
-
-isOddOrEven
-	n -> 'odd' or 'even'
+var isOddOrEven = (function (){
 	
+	var cache = {};
+	function checkOddOrEven(n){
+		console.log('processing ', n);
+		return n % 2 === 0 ? 'even' : 'odd';
+	}
+
+	return function(n){
+		if (typeof cache[n] === 'undefined')
+			cache[n] = checkOddOrEven(n);	
+		return cache[n];
+	}
+	
+})();
+
+
+
+function memoize(algoFn){
+	var cache = {};
+	return function(n){
+		if (typeof cache[n] === 'undefined')
+			cache[n] = algoFn(n);	
+		return cache[n];
+	}
+	
+}
+
+
 
